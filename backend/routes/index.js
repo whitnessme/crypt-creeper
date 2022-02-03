f8df61ef89b072b8699cb513e371849795a12b3e
Whitney Minson <whitnessprints@gmail.com>
2022-02-03 14:16:20 -0800

    finished backend setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,10 +6,7 @@ const { setTokenCookie } = require("../utils/auth");
 const { User } = require("../db/models/user");
 
 router.use("/api", apiRouter);
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
+
 
 
 module.exports = router;
