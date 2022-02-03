d6987a1ed04f87009fea02cccccc2a8a6be31a9f
Whitney Minson <whitnessprints@gmail.com>
2022-02-03 12:53:52 -0800

    auth middleware complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,14 @@
-const router = require('express').Router();
+const express = require("express");
+const router = express.Router();
+
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
+
+  
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const apiRouter = require('./api');
+const apiRouter = require("./api");
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie } = require("../utils/auth");
+const { User } = require("../db/models/user");
 
-router.use('/api', apiRouter);
-
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+router.use("/api", apiRouter);
+router.get("/hello/world", function (req, res) {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  res.send("Hello World!");
 });
+
 
 module.exports = router;
