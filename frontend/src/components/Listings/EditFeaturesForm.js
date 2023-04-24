a3b52b7f29eafdd6d6e506fdddf3c96a1a84ac83
Whitney Minson <whitnessprints@gmail.com>
2023-04-24 14:59:48 -0700

    fixed new feature redux store bug

--- frontend/src/components/Listings/EditFeaturesForm.js
+++ frontend/src/components/Listings/EditFeaturesForm.js
@@ -25,7 +25,9 @@ function EditFeaturesForm({ selectedHaunt, errors, setErrors, showErrors, setSho
 
   useEffect(() => {
     setCurrentFeatures(Object.values(features[showFeatures.toLowerCase()]))
-
+    console.log("current features", currentFeatures)
+    console.log("features", features)
+    
   }, [showFeatures, features])
 
   useEffect(() => {
